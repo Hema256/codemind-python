7f1c28d6bad786be207905a3223c41b814673e9d
Hema256 <84697657+Hema256@users.noreply.github.com>
2022-10-13 14:43:49 +0530

    n=int(input()) l=list(map(int,input().split())) c=0 c1=0 for i in l:     if i%2:         c+=1 for i in range(len(l)):     if i%2 and l[i]%2:             c1+=1 if c1==c:     print("True") else:     print("False")

---
+++ Strictly_ODD.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+c1=0
+for i in l:
+    if i%2:
+        c+=1
+for i in range(len(l)):
+    if i%2 and l[i]%2:
+            c1+=1
+if c1==c:
+    print("True")
+else:
+    print("False")
